c31cd4b0262e5b93b7aef3a060e61b0719e5693b
Daniel Terry <daniel-terry@lambdastudents.com>
2021-03-26 18:50:01 -0400

    Added basic routing for API with Node.js

--- index.js
+++ index.js
@@ -3,9 +3,20 @@ const express = require('express');
 const server = express();
 
 server.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Hello Scrubs');
 });
 
+server.get('/aboutMe', (req,res) => {
+    res.json({message:"Keeping backend fresh in my head."})
+})
+
+server.get('/about', (req,res) => {
+    res.status(200).send('<h1>About Us</h1>')
+})
+
+server.get('/contact', (req,res) => {
+    res.status(200).send('<h1>Contact us</h1>')
+})
 server.get('/hobbits', (req, res) => {
   const hobbits = [
     {
@@ -21,4 +32,16 @@ server.get('/hobbits', (req, res) => {
   res.status(200).json(hobbits);
 });
 
+server.post('/hobbits' , (req,res) => {
+    res.status(200).json({ url: '/hobbits', operation: 'POST' })
+})
+
+server.put('/hobbits', (req, res) => {
+    res.status(200).json({ url: '/hobbits', operation: 'PUT' });
+  });
+
+  server.delete('/hobbits', (req, res) => {
+    res.status(204);
+  });
+  
 server.listen(8000, () => console.log('API running on port 8000'));
